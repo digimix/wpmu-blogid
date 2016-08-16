01c4a1bc94729167d9377d7748df66aaa0829f2e
Digimix <michael@digimix.us>
2016-08-16 13:09:13 -0400

    added wp plugin meta

--- wpms_blogid.php
+++ wpms_blogid.php
@@ -1,4 +1,16 @@
 <?php
+/*
+Plugin Name:        wpmu-blogid
+Plugin URI:         https://github.com/digimix/wpmu-blogid
+Description:         Show blog ID, $blogid, as an admin column on WordPress multisites.
+Version:            1.0.0
+Author:             Digimix
+Author URI:         https://digimix.co/
+License:            MIT License
+License URI:        http://opensource.org/licenses/MIT
+*/
+
+<?php
 add_filter( 'wpmu_blogs_columns', 'do_get_id' );
 add_action( 'manage_sites_custom_column', 'do_add_columns', 10, 2 );
 add_action( 'manage_blogs_custom_column', 'do_add_columns', 10, 2 );
